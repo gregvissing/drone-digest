5e606853c785e9a6288541be27d518a12d43e582
Greg Vissing <greg.vissing@gmail.com>
2021-10-01 12:51:07 -0400

    Update PostsGrid.vue

--- components/grids/PostsGrid.vue
+++ components/grids/PostsGrid.vue
@@ -24,7 +24,7 @@ export default {
   props: {
     perRow: { type: Number, default: 3 },
     number: { type: Number, default: 0 },
-    order: { type: String, default: 'DESC' },
+    order: { type: String, default: 'ASC' },
     category: {
       type: Array,
       default() {
